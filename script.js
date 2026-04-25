2b9e70f19a72e5d5c4e70540c13b72218234e009
yarielisO <YARIELIS.ORTIZ-LOPEZ61@login.cuny.edu>
2026-04-25 01:18:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function nextPage() {
         let unique = document.querySelector(".unique");
         setTimeout(() => {
             unique.style.background = "#ccc";
-            unique.style.transform = "scale(0.5)
-                opacity:.4";
+            unique.style.transform = "scale(0.5);
+            unique.style.opacity = "0.4";
         }, 1000);
     }
     if (currentPage === 4) {
